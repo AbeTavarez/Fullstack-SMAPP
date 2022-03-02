ef9a442303f51670ac115c19e28051323d2f0d08
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-03-02 16:13:18 -0500

    create user and save to the db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
 
 // @route GET /api/users
 // @desc Test route
@@ -18,14 +20,56 @@ router.post(
   [
     check("name", "Name is required").not().isEmpty(),
     check("email", "Please include a valid email").isEmail(),
-    check("password", "Please enter a password with 8 or more characters.").isLength(8),
+    check(
+      "password",
+      "Please enter a password with 8 or more characters."
+    ).isLength(8),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()})
+      return res.status(400).json({ errors: errors.array() });
     }
-    res.send("user route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // check if user already exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        // If user already exists
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // user gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm",
+      });
+
+      // use the user variable to create a new user instance
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // encrypt password
+      const SALT = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, SALT);
+      await user.save(); // saving the new user
+
+      // return jwt
+      res.send("user created");
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: "Server error" });
+    }
   }
 );
 
