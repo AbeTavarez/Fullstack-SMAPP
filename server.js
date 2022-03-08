e0600e168b2cecfefaa9160b555a2c5195558df1
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-03-07 19:32:21 -0500

    morhan and helmet installed

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require("express");
-const connectDB = require('./config/db')
+const connectDB = require("./config/db");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
 // Connect to db
 connectDB();
 
 // Init Middleware
-app.use(express.json())
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(helmet());
+
 
 app.get("/", (req, res) => res.send("API Running"));
-
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
