49f01a40791f0acd6f3656fa09129171e44b625c
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-03-09 20:15:06 -0500

    register form created

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,87 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Register = () => {
-  return (
-    <div>
-      <h1>Register</h1>
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-export default Register
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("passwords dont match");
+    } else {
+      console.log(formData);
+    }
+  };
+
+  return (
+    <>
+      <h1>Sign Up</h1>
+      <p>
+        <i className="fas fa-user" /> Create an Account
+      </p>
+
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+        </div>
+
+        <div>
+          <input
+            type="text"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => handleChange(e)}
+            required
+          />
+          <small>This site uses Gravatar.</small>
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            value={password}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            minLength="6"
+            value={password2}
+            onChange={(e) => handleChange(e)}
+          />
+        </div>
+        <input type="submit" value="Register" />
+      </form>
+      <p>
+        Already have an account?
+        <a>Sign In</a>
+      </p>
+    </>
+  );
+};
+
+export default Register;
