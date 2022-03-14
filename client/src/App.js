286f351d5cffd62554aaabd2ce174f1c34aa5b57
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-03-13 22:27:42 -0400

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
-
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-     <h1>App</h1>
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+
+      <section>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
