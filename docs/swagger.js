9d7fca2abffa8e575673ce1e7af1c81e70dc246e
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-03-13 22:26:22 -0400

    swagger doc

--- docs/swagger.js
+++ docs/swagger.js
@@ -6,10 +6,15 @@ module.exports = {
       "Full CRUD API for a social media app. Features: Users registration and authentication. Posts functionality, likes, comments etc.",
     version: "0.1.9",
   },
+  server: {},
   paths: {
     "/api/auth": {
       get: {
-        summary: "Get the authorized user with the user id.",
+        summary: "Returns authorized user.",
+        description: "Get the authorized user with the user id.",
+        responses: {
+            "200": "ok"
+        }
       },
       post: {
         summary: "Authenticate an user with an email and a password, and get a JWT token back.",
